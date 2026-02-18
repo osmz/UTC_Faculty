8bd6606d6af10030bc33f032745e3274801e6a91
Oscar S Morales Z - Toshiba <osmorales1502@gmail.com>
2026-02-18 14:42:50 -0500

    Modify formatearFecha

--- js/api.js
+++ js/api.js
@@ -53,7 +53,7 @@ function formatearFecha(fechaISO) {
     const year = fecha.getFullYear();
     const mes = String(fecha.getMonth() + 1).padStart(2, '0');
     const dia = String(fecha.getDate()).padStart(2, '0');
-    return `${year}/${mes}/${dia}`;
+    return `${dia}/${mes}/${year}`;
   } catch (e) {
     return fechaISO;
   }
